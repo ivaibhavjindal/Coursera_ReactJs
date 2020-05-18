179670db23680bb17e5f84779b432cb887d61980
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-05-18 22:29:24 +0530

    Single Page App 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,14 +4,20 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        dishes: DISHES,
-        selectedDish: null
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
@@ -24,7 +30,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return(
-                <Home />
+                <Home dish={ this.state.dishes.filter((dish) => dish.featured)[0] }
+                    promotion={ this.state.promotions.filter((promo) => promo.featured)[0] }
+                    leader={ this.state.leaders.filter((leader) => leader.featured)[0] }
+                />
             );
         }
         return (
@@ -33,6 +42,7 @@ class Main extends Component {
             <Switch>
                 <Route path="/home" component={ HomePage } />
                 <Route exact path='/menu' component={ () => <Menu dishes={this.state.dishes} />} />
+                <Route exact path='/contactus' component={Contact} />} />
                 <Redirect to="/home" />
             </Switch>
             <Footer />
